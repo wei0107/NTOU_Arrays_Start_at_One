59d40f5346fdc300542126256c5e1b9a9b1d20e0
tana0101 <98708317+tana0101@users.noreply.github.com>
2022-02-19 02:48:25 +0800

    Add files via upload

--- codebook/Graph/Kruskal’s algorithm 最小生成樹.cpp
+++ codebook/Graph/Kruskal’s algorithm 最小生成樹.cpp
@@ -1,10 +1,12 @@
 /*Kruskal’s algorithm 最小生成樹*/
 //搭配 Disjoint Set(Union-Find)
 
+
+
 struct Edge {
-    int u, v, w;    // 點 u 連到點 v 並且邊權為 w
+    int u, v, w;  // 點 u 連到點 v 並且邊權為 w
     friend bool operator<(const Edge& lhs, const Edge& rhs) {
-        return lhs.w > rhs.w;    //兩條邊比較大小用邊權比較
+        return lhs.w > rhs.w;//兩條邊比較大小用邊權比較
     }
 };
 
@@ -13,8 +15,9 @@ int kruskal(int m){
     int tot = 0;
     for (int i = 0; i < m ; i++) {
         if (find(graph.top().u) != find(graph.top().v)) { // 如果兩點未聯通
-            merge(graph.top().u, graph.top().v);    // 將兩點設成同一個集合
-            tot += graph.top().w;    // 權重加進答案
+            merge(graph.top().u, graph.top().v);    
+               // 將兩點設成同一個集合
+            tot += graph.top().w;  // 權重加進答案
         }
         graph.pop();
     }
